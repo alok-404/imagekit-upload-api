60a0182dba05fc29b7392d710eec6eedb159a071
alok-404 <alokchandra2621@gmail.com>
2026-06-25 23:06:52 +0530

    fix: configure CORS to allow frontend (Vercel) instead of backend origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,10 @@ const cors = require("cors")
 connectToDB();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin:"https://imagekit-upload-api-eight.vercel.app/",
+    credentials: true
+}));
 app.use(express.json());
 
 app.use("/",imageRoutes);
